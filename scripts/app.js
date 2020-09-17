48f3ea48caa5b5910e97629ab8ec2f32e12038a6
andres Snitcofsky <andres@soviet.com.ar>
2020-09-17 19:51:32 -0300

    updated csv url

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,6 @@
 var $ = jQuery.noConflict();
 //var url_csv = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRQ9TKGRXDRqbxc16L00uA3A31QimdVG0CNXt1tQjzRSf6ezbftUacb1zauCmhEj4qvJIfIRBEO0rbu/pub?gid=1901807811&single=true&output=csv";
-var url_csv = "/data/medicamentos.csv";        
+var url_csv = "https://visualizando.github.io/data/medicamentos.csv";        
 var genApp = angular.module('genApp', ['app.routes', 'app.core' , 'app.services', 'app.auth', 'ngRoute']);
 // genApp.constant('apiConstants',{ API_URL: 'https://1fxtrwjbb5.execute-api.us-east-1.amazonaws.com/dev/api' });
 genApp.run(function($rootScope, $timeout, $location) {
